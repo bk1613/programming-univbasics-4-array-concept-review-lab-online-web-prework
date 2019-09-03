6730334860aa048ded43ea0309776911cd03ae9e
Brian Kao <bk1613@nyu.edu>
2019-09-03 13:20:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
   counter = 0
   
   
-  while counter < array.length {
-    if array[counter] == value_to_find
-      return array.index(value_to_find)
+  while counter < array.length do
+    
     counter += 1
   }
 end
