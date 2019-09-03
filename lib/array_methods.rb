072963be71a06d1db8f89b9d492b2d740fc88bff
Brian Kao <bk1613@nyu.edu>
2019-09-03 13:11:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,8 +15,10 @@ def find_max_value(array)
   max = 0
   while array[counter] {
     if array[counter] > max
+      max = array[counter]
     counter += 1
   }
+  return max
 end
 
 def find_min_value(array)
